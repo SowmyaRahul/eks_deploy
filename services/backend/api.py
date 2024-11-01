f455eea7058f4cae0479d5734b84181cfa3af898
Sowmya Pasunoori <reddy.pasunoori03@gmail.com>
2024-11-01 15:14:12 -0400

    rahul commit

--- services/backend/api.py
+++ services/backend/api.py
@@ -83,6 +83,9 @@ def delete_note(id):
 
     return jsonify({'message': 'Note deleted successfully!'}), 200
 
+@app.route('/api/notes/rahul', methods=['GET'])
+def yoga():
+    return jsonify({'message': 'Rahul experienced YOGA successfully!'}), 200
 
 
 if __name__ == '__main__':
